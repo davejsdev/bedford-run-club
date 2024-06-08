b80b00a6a211fdd9bd4b1774cb2cd7181d9ed018
David Leger <david.leger@getkard.com>
2024-06-08 20:14:51 -0300

    use fa logos

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,18 +1,18 @@
 
 import Image from "astro/components/Image.astro"
 import classes from './SocialLinks.module.scss'
-import stravaLogo from "../../assets/strava-logo.svg"
-import instagramLogo from "../../assets/instagram-logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faStrava } from "@fortawesome/free-brands-svg-icons";
 
 const links = [
 	{
 		href: "https://www.strava.com/clubs/935968",
-		icon: stravaLogo,
+		icon: faStrava,
 		alt: "Strava"
 	},
 	{
 		href: "https://www.instagram.com/bedfordrunclub",
-		icon: instagramLogo,
+		icon: faInstagram,
 		alt: "Instagram"
 	}
 ];
@@ -21,7 +21,7 @@ const SocialLinks = () => (
 	<ul className={classes.navMenu}>
 		{links.map(link => (<li className={classes.navItem}>
 			<a className={classes.navLink} href={link.href} target="_blank" rel="noopener noreferrer">
-				<img className={classes.icon} src={link.icon.src} alt={link.alt} />
+			<FontAwesomeIcon icon={link.icon} aria-label={link.alt} />
 			</a>
 		</li>))}
 	</ul>
